b9152a49ddeb365a4b1092deca9a4c1c68dfd407
Anonym <anonym1120@gmail.com>
2022-06-20 03:18:25 +0800

    add axios.get sample

--- VueProject/src/views/AboutView.vue
+++ VueProject/src/views/AboutView.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <button @click="say(axiosData)" class="btn btn-primary">submit</button>
   </div>
 </template>
+
+<script>
+// import { ref, onMounted } from 'vue'
+// import axios from 'axios';
+
+export default {
+  setup() {
+    //  const axiosData = ref([]);
+    //  const url = ref('https://localhost:XXXX/weatherforecast');
+
+    // onMounted(() => {
+
+    //   axios.get(url.value)
+    //   .then((response) => {
+    //     axiosData.value = response
+    //   })
+
+    // })
+    
+
+    return { }
+  }
+}
+</script>
